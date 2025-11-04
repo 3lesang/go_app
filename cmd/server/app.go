aa7ea536e5814a66bc65ea65b1c876f39b4c9715
Sang Le <3lesang@gmail.com>
2025-11-04 08:41:36 +0700

    update cors

--- cmd/server/app.go
+++ cmd/server/app.go
@@ -22,7 +22,7 @@ func Serve() {
 
 	app.Use(logger.New())
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173,http://localhost:3000",
+		AllowOrigins:     "http://localhost:5173,http://localhost:3000,https://admin.senhome.vn,https://web-dev.senhome.vn,https://senhome.vn",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		ExposeHeaders:    "Content-Length, Authorization",
